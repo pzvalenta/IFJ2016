ce67c2eacfc9ceef078707222abffc51d99d96fb
EliskaK <elis.kadlecova@gmail.com>
2016-10-12 21:49:36 +0200

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -17,23 +17,29 @@
 
 
 
-#define T_END       001
+#define T_END           001
 
-#define T_EQUAL     401
-#define T_ADD       402
-#define T_PLUS      403
-#define T_GREAT     405
-#define T_GEQUAL    406
-#define T_MINUS     407
-#define T_LESS      409
-#define T_LEQUAL    410
-#define T_MUL       411
-#define T_EXCLAIM   414
-#define T_SLASH     415
+#define T_EQUAL         401
+#define T_ADD           402
+#define T_PLUS          403
+#define T_GREAT         405
+#define T_GEQUAL        406
+#define T_MINUS         407
+#define T_LESS          409
+#define T_LEQUAL        410
+#define T_MUL           411
+#define T_EXCLAIM       414
+#define T_SLASH         415
 
-#define T_LBRACKET  501
-#define T_RBRACKET  502
-
+#define T_LBRACKET      501
+#define T_RBRACKET      502
+#define T_LCBRACKET     503
+#define T_RCRBRACKET    504
+#define T_LSBRACKET     505
+#define T_RSBRACKET     506
+#define T_SEMICLN       507
+#define T_COMMA         508
+#define T_DOT           509
 
 
 #endif // SCANNER
