c9c8d40a9ce89f861014a1aad067ee74f8fca55c
Petr Valenta <justreverse@gmail.com>
2016-10-12 15:27:20 +0200

    Added example for testers

---
+++ testing/input/example1.java
@@ -0,0 +1,14 @@
+class Main
+{
+	static int x;
+	static void run()
+	{
+		String str1;
+		str1 = "Toto je nejaky text";
+		String str2;
+		str2 = str1 + ", ktery jeste trochu obohatime";
+		Main.x = ifj16.find(str2, "text");
+		ifj16.print("Pozice retezce \"text\" v retezci str2: " + x + "\n");
+		Game.play(str1);
+	} // end of static void run()
+} // end of class Main
