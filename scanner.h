2a6827dd65db63b11f002c021e5d30f7d2da187e
EliskaK <elis.kadlecova@gmail.com>
2016-10-12 21:17:43 +0200

    Create scanner.h

---
+++ scanner.h
@@ -0,0 +1,39 @@
+/** Prekladac jazyka IFJ16
+ *  Projekt do predmetu IFJ a IAL
+ *  VUT FIT Brno, 2016
+ *
+ *  Tym 051, varianta a/2/I
+ *  Autori:
+ *      Petr Valenta
+ *      Eliska Kadlecova
+ *      Daniel Doubek
+ *      Martin Kovarik
+ *      Petr Jares
+ *
+ */
+
+#ifndef SCANNER
+#define SCANNER
+
+
+
+#define T_END       001
+
+#define T_EQUAL     401
+#define T_ADD       402
+#define T_PLUS      403
+#define T_GREAT     405
+#define T_GEQUAL    406
+#define T_MINUS     407
+#define T_LESS      409
+#define T_LEQUAL    410
+#define T_MUL       411
+#define T_EXCLAIM   414
+#define T_SLASH     415
+
+#define T_LBRACKET  501
+#define T_RBRACKET  502
+
+
+
+#endif // SCANNER
