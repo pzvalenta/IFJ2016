ad1cfeeafcc5c56f67f591512153fe61f5a1eb69
EliskaK <elis.kadlecova@gmail.com>
2016-10-14 23:15:38 +0200

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -15,6 +15,12 @@
 #ifndef SCANNER
 #define SCANNER
 
+#define T_IDENT         101
+#define T_KEY           200
+
+#define T_NUMBER_I      301
+#define T_NUMBER_D      302
+#define T_STRING        303
 
 
 #define T_END           001
@@ -34,12 +40,21 @@
 #define T_LBRACKET      501
 #define T_RBRACKET      502
 #define T_LCBRACKET     503
-#define T_RCRBRACKET    504
+#define T_RCBRACKET     504
 #define T_LSBRACKET     505
 #define T_RSBRACKET     506
 #define T_SEMICLN       507
 #define T_COMMA         508
 #define T_DOT           509
 
+typedef struct _Token {
+	char id; //na to nase id nam staci jeden byte
+	union{ //union zabira tolik mista v pameti jako jeho nejvetsi prvek, smi se pouzit jenom jeden
+		long i; //int
+		double f; //float
+		char * s; //string
+		//pomoci tohoto pointeru budeme predavat bud text, nebo ukazatel do tabulky hodnot
+	}data;
+} Token;
 
 #endif // SCANNER
