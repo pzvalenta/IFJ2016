327a24394f4b5e3fd2bdb48cbbe4cafaa75da990
Petr Valenta <justreverse@gmail.com>
2016-10-15 18:54:34 +0200

    DestroyToken function

--- src/token.c
+++ src/token.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "token.h"
+#include "str.h"
+#include "scanner.h"
 
 struct _Token{
 	char id; // na to nase id nam staci jeden byte
@@ -12,7 +14,7 @@ struct _Token{
 };
 
 Token *newToken(){
-	Token *ret = malloc(sizeof(Token));
+	Token *ret = (String *) malloc(sizeof(Token));
 	if (ret == NULL){
 		fprintf("Not enought memory, can't alloc.\n",stderr);
 		return NULL;
@@ -22,14 +24,12 @@ Token *newToken(){
 }
 
 
-void destroyToken(Token *token){ //TODO
+void destroyToken(Token *token){
 	if (token != NULL){
-		if (token->id == 101 || token->id ==  ............) /// jestlize jde o token ktery ma v union zabrany ukazatel s, musim uvolnit tento ukazatel
-			free(token->s)
-		free(token)
+		if (token->id == T_STRING) /// jestlize jde o token ktery ma v union zabrany ukazatel s, musim uvolnit tento ukazatel
+			destroyString(token->s);
+		else if (token->id == T_IDENT)
+			destroySimTableNode(token->s);
+		free(token);
 	}
 }
-	
-	
-	
-	
