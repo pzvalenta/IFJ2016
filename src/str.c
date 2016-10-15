8756611b2b2446102d3dd4b0be9538c71668b6fa
Petr Valenta <justreverse@gmail.com>
2016-10-15 18:54:51 +0200

    Complete overhaul - string is now a struct

--- src/str.c
+++ src/str.c
@@ -5,29 +5,50 @@
 
 #define BASE_STRING_SIZE 4
 
+struct _String{
+	int len;
+	int size;
+	char * data;
+};
+
+
 // returns pointer to an array of chars BASE_STRING_SIZE long
-char *newString(int size){
-	char *ret = (char *) malloc(size*sizeof(char));
+String *newString(int size){
+	String *ret = (String *) malloc(sizeof(String));
 	if (ret == NULL) return NULL;
+
+	ret->data = (char *) malloc(sizeof(char));
+	if (ret->data == NULL){
+		return NULL;
+		destroyString(ret);
+	}
+
+	ret->size = size;
+	ret->len = 0;
 	return ret;
 }
 
 // doubles the size of a char array
-char *resizeString(char *string){
-	char *ret = (char *) realloc(string, 2*strlen(string));
-	if (ret == NULL){
-		char *ret2 = (char *) malloc(2*strlen(string));
-		if (ret2 == NULL)
-			return NULL;
-		else
-			return ret2;
-	} 
-	else 
-		return ret;
-}
+// char *resizeString(char *string){
+// 	char *ret = (char *) realloc(string, 2*(strlen(string)+1));
+// 	if (ret == NULL){
+// 		char *ret2 = (char *) malloc(2*(strlen(string)+1)); //TODO je toto nunte?
+// 		if (ret2 == NULL)
+// 			return NULL;
+// 		else
+// 			return ret2;
+// 	}
+// 	else
+// 		return ret;
+// }
 
-char *copyString(char *old){
-	char *ret = newString(strlen(old));
-	strcpy(ret, old);
-	return ret;
+// char *copyString(char *old){
+// 	char *ret = newString(strlen(old));
+// 	strcpy(ret, old);
+// 	return ret;
+// }
+
+//TODO
+void appendChar(String *str){
+
 }
