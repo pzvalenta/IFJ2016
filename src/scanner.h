50c739ca09a2421c0fe4f8cd1b6c6f6d24f47c91
Petr Valenta <justreverse@gmail.com>
2016-10-16 23:17:01 +0200

    removed token redeclaration

--- src/scanner.h
+++ src/scanner.h
@@ -47,14 +47,4 @@
 #define T_COMMA         508
 #define T_DOT           509
 
-typedef struct _Token {
-	char id; //na to nase id nam staci jeden byte
-	union{ //union zabira tolik mista v pameti jako jeho nejvetsi prvek, smi se pouzit jenom jeden
-		long i; //int
-		double f; //float
-		char * s; //string
-		//pomoci tohoto pointeru budeme predavat bud text, nebo ukazatel do tabulky hodnot
-	}data;
-} Token;
-
 #endif // SCANNER
