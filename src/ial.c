eedfbd17b271eb91dc6697d0793a27789429b51f
Petr Valenta <justreverse@gmail.com>
2016-10-17 16:09:22 +0200

    now working

--- src/ial.c
+++ src/ial.c
@@ -1,19 +1,7 @@
 #include "ial.h"
 // Symbol Table implementation
 
-struct _SymTableNode{
-	String *name; // identifikator a zaroven klic
-	char id; // na to nase id nam staci jeden byte
 
-	union{ // union zabira tolik mista v pameti jako jeho nejvetsi prvek, smi se pouzit jenom jeden
-		long i; // int
-		double f; // float
-		String * s; // string
-	} data;
-
-	SymTableNode *left;
-	SymTableNode *right;
-};
 
 SymTableNode *newSymTableNode(Token *token, String *str){
 	SymTableNode *ret = malloc(sizeof(SymTableNode));
@@ -22,7 +10,7 @@ SymTableNode *newSymTableNode(Token *token, String *str){
 		return NULL;
 	}
 
-	ret->name = copyString(str);
+	ret->name = str;
 	ret->id = token->id;
 	ret->data.s = NULL;
 
@@ -49,6 +37,82 @@ SymTableNode *insertSymTableNode(SymTableNode *root, SymTableNode *node){
 	return root;
 }
 
+void deleteSymTableNode(SymTableNode *root, SymTableNode *node){
+	if (root == NULL || node == NULL) return NULL;
+	int cmp = strcmp(node->name->data, root->name->data);
+	if (cmp > 0)
+		deleteSymTableNode(root->right, node);
+	else if (cmp < 0)
+		deleteSymTableNode(root->left, node);
+	else { //root == node
+		if (node->right == NULL && node->left == NULL){
+			destroySymTableNode(node);
+			root = NULL;
+		}
+		else if (node->right != NULL && node->left == NULL){
+			replaceSymTableNode(node, node->right);
+		}
+		else if (node->right == NULL && node->left != NULL){
+			replaceSymTableNode(node, node->left);
+		}
+		else if (node->right != NULL && node->left != NULL){
+			SymTableNode *tmp = findMaxNode(node->left);
+			tmp->right = node->right;
+			replaceSymTableNode(node, tmp);
+			deleteSymTableNode(root, tmp);
+		}
+	}
+	// if (node == NULL) return NULL;
+	//
+	// if (node->right == NULL && node->left == NULL){
+	// 	destroySymTableNode(node);
+	// 	node = NULL;
+	// }
+	// else if (node->right != NULL && node->left == NULL){
+	// 	replaceSymTableNode(node, node->right);
+	// }
+	// else if (node->right == NULL && node->left != NULL){
+	// 	replaceSymTableNode(node, node->left);
+	// }
+	// else if (node->right != NULL && node->left != NULL){
+	// 	SymTableNode *tmp = findMaxNode(node->left);
+	// 	tmp->right = node->right;
+	// 	replaceSymTableNode(node, tmp);
+	// 	deleteSymTableNode(tmp);
+	// }
+	// return;
+}
+
+void destroySymTableNode(SymTableNode *node){
+	if (node->id == T_STRING)
+		destroyString(node->data.s);
+	destroyString(node->name);
+	free(node);
+	node = NULL;
+}
+
+void destroySymTable(SymTableNode *root){
+	if (root->left != NULL)
+		destroySymTable(root->left);
+	if (root->right != NULL)
+		destroySymTable(root->right);
+	destroySymTableNode(root);
+}
+
+void replaceSymTableNode(SymTableNode *out, SymTableNode *in){
+	if (out->id == T_STRING)
+		destroyString(out->data.s);
+	destroyString(out->name);
+
+	out->data = in->data;
+	out->name = in->name;
+	out->id = in->id;
+	out->left = in->left;
+	out->right = in->right;
+	free(in);
+	in = NULL;
+}
+
 void printInorder(SymTableNode *node){
 	if (node == NULL) return;
 	printInorder(node->left);
@@ -56,7 +120,16 @@ void printInorder(SymTableNode *node){
 	printInorder(node->right);
 }
 
+SymTableNode *findMaxNode(SymTableNode *root){
+	if (root == NULL) return NULL;
+
+	if (root->right != NULL) return findMaxNode(root->right);
+	else return root;
+}
+
 SymTableNode *searchSymTable(SymTableNode *root, char *exp){
+	if (root == NULL) return NULL;
+
 	int a = strcmp(exp, root->name->data);
 	if (a == 0) return root;
 	else if (a > 0) return searchSymTable(root->right, exp);
@@ -65,13 +138,19 @@ SymTableNode *searchSymTable(SymTableNode *root, char *exp){
 	return NULL; // not found
 }
 
+SymTableNode *findParent(SymTableNode *root, SymTableNode *node){
+	if (root == NULL || node == NULL) return NULL;
+
+	if (root == node) return NULL; //no parent, it is root
+	else if (root->left == node) return root;
+	else if (root->right == node) return root;
+
+	int a = strcmp(node->name->data, root->name->data);
+	if (a > 0) return findParent(root->right, node);
+	else return findParent(root->left, node);
+}
+
 // Pomocny zasobnik
-
-struct _SymStackItem{
-	SymTableNode *table;
-	SymStackItem *next;
-};
-
 SymStackItem *newSymStackItem(){
 	SymStackItem *ret = malloc(sizeof(SymStackItem));
 	if (ret == NULL){
@@ -90,8 +169,22 @@ SymStackItem *pushSymStackItem(SymStackItem *item, SymStackItem *stack){
 }
 
 SymStackItem *popSymStackItem(SymStackItem *stack){
+	if (stack == NULL) return NULL;
 	SymStackItem *ret = stack;
 	stack = ret->next;
 	ret->next = NULL;
 	return ret;
+}
+
+void destroySymStack(SymStackItem *stack){
+	if (stack->next != NULL)
+		destroySymStack(stack->next);
+	else
+		destroySymStackItem(stack);
+}
+
+void destroySymStackItem(SymStackItem *item){
+	destroySymTable(item->table);
+	free(item);
+	item = NULL;
 }
--- src/ial.h
+++ src/ial.h
@@ -8,15 +8,42 @@
 #include "str.h"
 #include "token.h"
 
-typedef struct _SymTableNode SymTableNode;
+typedef struct SymTableNode SymTableNode;
 
-typedef struct _SymStackItem SymStackItem;
+struct SymTableNode{
+	String *name; // identifikator a zaroven klic
+	char id; // na to nase id nam staci jeden byte
 
+	union{ // union zabira tolik mista v pameti jako jeho nejvetsi prvek, smi se pouzit jenom jeden
+		long i; // int
+		double f; // float
+		String * s; // string
+	} data;
+
+	SymTableNode *left;
+	SymTableNode *right;
+};
+
+typedef struct SymStackItem SymStackItem;
+
+struct SymStackItem{
+	SymTableNode *table;
+	SymStackItem *next;
+};
+
+
+//////////////EXTERNAL FUNCTIONS//////////////
 SymTableNode *newSymTableNode(Token *token, String *str);
 
 SymTableNode *insertSymTableNode(SymTableNode *root, SymTableNode *node);
 
+void deleteSymTableNode(SymTableNode *root, SymTableNode *node);
+
+void replaceSymTableNode(SymTableNode *out, SymTableNode *in);
+
 void printInorder(SymTableNode *node);
+
+SymTableNode *findMaxNode(SymTableNode *root);
 
 SymTableNode *searchSymTable(SymTableNode *root, char *exp);
 
@@ -25,6 +52,15 @@ SymStackItem *newSymStackItem();
 SymStackItem *pushSymStackItem(SymStackItem *item, SymStackItem *stack);
 
 SymStackItem *popSymStackItem(SymStackItem *stack);
+
+void destroySymStack(SymStackItem *stack);
+
+void destroySymStackItem(SymStackItem *item);
+
+//////////////INTERNAL FUNCTIONS//////////////
+void destroySymTableNode(SymTableNode *node);
+
+void destroySymTable(SymTableNode *root);
 
 
 #endif
