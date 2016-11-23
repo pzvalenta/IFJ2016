730a53831fa3e104f6c2c9622f9ab4ad08ab36ad
Petr Valenta <justreverse@gmail.com>
2016-11-23 16:22:22 +0100

    added function for readable prints of scanner output

---
+++ src/debug.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_DEBUG
+#define HEADER_DEBUG
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "str.h"
+#include "scanner.h"
+#include "main.h"
+
+void dprint(int token, String *data);
+
+#endif
