37b09a256d45029a6b86ac75d0b6e94a1187788e
EliskaK <elis.kadlecova@gmail.com>
2016-11-23 14:45:06 +0100

    updated list of instruction

--- src/ilist.h
+++ src/ilist.h
@@ -17,23 +17,47 @@
  #define I_STOP         0     //stop
 
  #define I_ADD          1     //scitani
+/* list_insert_last(*List instrukci, I_ADD, scitanec, scitanec, vysledek)*/
  #define I_SUB          2     //odcitani
+/* list_insert_last(*List instrukci, I_SUB, mensenec, mensitel, vysledek)*/
  #define I_MUL          3     //nasobeni
+/* list_insert_last(*List instrukci, I_MUL, cinitel, cinitel, vysledek)*/
  #define I_DIV          4     //deleni
+/* list_insert_last(*List instrukci, I_DIV, delenec, delitel, vysledek)*/
 
  #define I_GREAT        5     //vetsi nez >
+ /* list_insert_last(*List instrukci, I_GREAT, 1. porovnavany, 2. porovnavany, vysledek)*/
  #define I_LESS         6     //mensi nez <
+ /* list_insert_last(*List instrukci, I_LESS, 1. porovnavany, 2. porovnavany, vysledek)*/
  #define I_GREQ         7     //vetsirovno >=
+ /* list_insert_last(*List instrukci, I_GREQ, 1. porovnavany, 2. porovnavany, vysledek)*/
  #define I_LESEQ        8     //mensirovno <=
+ /* list_insert_last(*List instrukci, I_LESEQ, 1. porovnavany, 2. porovnavany, vysledek)*/
  #define I_EQUAL        9     //rovna se ==
+ /* list_insert_last(*List instrukci, I_EQUAL, 1. porovnavany, 2. porovnavany, vysledek)*/
  #define I_NEQUAL       10    //nerovna se !=
+/* list_insert_last(*List instrukci, I_NEQUAL, 1. porovnavany, 2. porovnavany, vysledek)*/
+
+ #define I_GOTO         11    //skok
+ /* list_insert_last(*List instrukci, I_GOTO,??,adresa kam skocit, )*/
+ #define I_IFGOTO       12    //podmineny skok
+ /* list_insert_last(*List instrukci, I_IFGOTO, 1/0 = skok/neskok, adresa kam skocit, )*/
+ #define I_PUSH         13    //push
+/* list_insert_last(*List instrukci, I_PUSH, co se pushuje, , )*/
+
+
+
+
 
  // #define I_FIND         9     //find
  // #define I_INC          10    //inkrementace
  // #define I_DEC          11    //dekrementace
  // #define I_NOT          12    //not
- // #define I_IFGOTO       13    //podmineny skok
- // #define I_GOTO         14    //skok
+ // #define I_IFGOTO       13    //podmineny skok !
+
+ // input
+ // read (3 ruzne) (zatim vynechat)
+ // print
 
  #define I_LABEL        17    //navesti
 
