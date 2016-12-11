00ce3f8445878a8232d30885c2219bc331855cb6
Petr Valenta <justreverse@gmail.com>
2016-12-11 22:21:56 +0100

    opraven voidcall funkce

--- parser.c
+++ parser.c
@@ -241,7 +241,9 @@ int function_rule() {
   token = token->next;
   if (token->id == T_RBRACKET){
     fprintf(stderr, "INFO: call funkce bez parametru\n");
-    if (params->len > 1) return E_TYP;
+    if (SECOND_RUN){
+      if (params->len > 1) return E_TYP;
+    }
 
   }else{
     int paramPos = 1;
